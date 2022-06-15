667db0ccbc519efaae3051a75d9dcebe10309847
ehtovk <vicaetano_barros@hotmail.com>
2022-06-15 14:30:47 -0300

    Added the function showCards

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,33 @@
+let arr;
+let quantidade;
+
+startGame();
+
 function startGame() {
-   const quantidade = prompt("Digite a quantidade de cartas de 4 a 14:");
+   quantidade = prompt("Digite a quantidade de cartas par de 4 a 14:");
 
    if (quantidade <= 14 && quantidade >= 4 && quantidade%2 === 0) {
-    alert("Seu numero é valido");
+      showCards();
    } else {
-    alert("Seu numero não é valido")
-    startGame();
+      quantidade = 0;
+      startGame();
    }
 }
-startGame();
+
+function showCards() {
+   const elemento = document.querySelector(".cards");
+   
+   for(let i = 0; i < quantidade; i++) {
+      elemento.innerHTML += 
+      `        <div id="card${i}" class="card" data-identifier="card">
+                  <div class="face " data-identifier="back-face" >
+                     <img src="assets/img/front 1.png" alt="">
+                  </div>
+                  <div class="face turned-off" data-identifier="front-face">
+                     <img src="assets/img/papagaio1.gif" alt="">
+                  </div>
+               </div>
+      `
+   }
+}
+
