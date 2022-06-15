9f5d97c5d87e684bc68315674a7e5cf2c3f4be0e
ehtovk <vicaetano_barros@hotmail.com>
2022-06-15 17:58:54 -0300

    Created a raffle to cards and added a function to turn around the card on click

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,8 @@
 let arr;
 let quantidade;
+let papagaios;
+let content;
+let verifier;
 
 startGame();
 
@@ -12,22 +15,52 @@ function startGame() {
       quantidade = 0;
       startGame();
    }
+   
+}
+
+function mix() {
+   return 0.5 - Math.random();
 }
 
 function showCards() {
    const elemento = document.querySelector(".cards");
+   content_Parrots = [];
+
+   papagaios  = [0,1,2,3,4,5,6];
+
+   papagaios.sort(mix);
+
+   for(let i = 0; i < quantidade/2; i++) {
+      content_Parrots.push(papagaios[i]);
+      content_Parrots.push(papagaios[i]);
+   }
+
+   content_Parrots.sort(mix);
    
    for(let i = 0; i < quantidade; i++) {
+
+      let papagaio = content_Parrots[i];
       elemento.innerHTML += 
-      `        <div id="card${i}" class="card" data-identifier="card">
-                  <div class="face " data-identifier="back-face" >
+      `        <div id="card${i}" onclick="turnAroundOnClick(this)"  class="card" data-identifier="card">
+                  <div class="face parrot" data-identifier="back-face" >
                      <img src="assets/img/front 1.png" alt="">
                   </div>
-                  <div class="face turned-off" data-identifier="front-face">
-                     <img src="assets/img/papagaio1.gif" alt="">
+                  <div class="face parrot-gif turned-off" data-identifier="front-face">
+                     <img src="assets/img/papagaio${papagaio}.gif" alt="">
                   </div>
                </div>
       `
    }
 }
+
+function turnAroundOnClick(place) {
+   turnCards(place);
+}
+
+function turnCards(place) {
+   place.querySelector(".parrot").classList.toggle("turned-off");
+   place.querySelector(".parrot-gif").classList.toggle("turned-off");
+}
+
+
 
